3dc6fa0d9ed4144e8356b23deb0503bfe626e759
Tara / Tingyu Liu <80751447+drunken-boat@users.noreply.github.com>
2024-01-11 03:47:21 +0800

    Create 181_EmployeesEarnMoreThanManager.sql

---
+++ sql/181_EmployeesEarnMoreThanManager.sql
@@ -0,0 +1,17 @@
+
+/*
+table: Employee
+output: name alias: Employee
+condition: Employee's salary > mployee(managerID = emloyee's)salary
+
+algo: left join
+1. join on Id & managerId
+2. compare salary, based on condition
+*/
+
+SELECT e1.name AS Employee 
+FROM Employee as e1
+LEFT JOIN Employee as e2
+ON e1.ManagerID = e2.Id
+WHERE e1.Salary > e2.Salary
+;
