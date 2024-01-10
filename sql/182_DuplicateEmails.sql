97032dff5fe0042d5439aa2b6e3809b6a766d864
Tara / Tingyu Liu <80751447+drunken-boat@users.noreply.github.com>
2024-01-11 03:05:04 +0800

    Update 182_DuplicateEmails.sql

--- sql/182_DuplicateEmails.sql
+++ sql/182_DuplicateEmails.sql
@@ -6,6 +6,11 @@ return: emails
 that are duplicate -> id different, email same
 any order
 algo: self join
+
+Reference:
+1. https://blog.csdn.net/weixin_43972154/article/details/123825607
+2. https://www.w3schools.com/sql/sql_join_self.asp
+
 */
 
 -- Runtime 650ms
